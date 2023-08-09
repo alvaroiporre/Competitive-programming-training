1fe8875ca133be42d64b756a5677ff7cd4dd819d
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-08-08 23:48:48 -0400

    Add solution atcoder ABC-045-C

---
+++ Atcoder/ABC-045/c.py
@@ -0,0 +1,19 @@
+from sys import stdin, stdout
+n = stdin.readline().strip()
+
+l = len(n)
+
+s = 0
+for i in range(1 << (l - 1)):
+    pos = l
+    for j in range(l):
+        if i&(1<<(j)):
+            x = l - j - 1
+            s += int(n[x:pos])
+            pos = x
+    if pos > 0:
+        s += int(n[0:pos])
+stdout.write(str(s) + '\n')
+
+
+#AC
