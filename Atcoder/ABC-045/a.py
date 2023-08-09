ede6e3466cda65bfb1c6d0f7827831d3e154f6b9
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-08-08 22:10:56 -0400

    Add solution atcoder ABC-045-A

---
+++ Atcoder/ABC-045/a.py
@@ -0,0 +1,9 @@
+from sys import stdin, stdout
+
+a = int(stdin.readline())
+b = int(stdin.readline())
+h = int(stdin.readline())
+area = int((b + a) * (h / 2))
+stdout.write(str(area) + '\n')
+
+#AC
