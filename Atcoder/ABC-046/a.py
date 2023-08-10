04ccefea679920f1021e7fb9c8c92edb021fa3f4
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-08-09 21:15:21 -0400

    Add solution atcoder ABC-046-A

---
+++ Atcoder/ABC-046/a.py
@@ -0,0 +1,9 @@
+from sys import stdin, stdout
+[a, b, c] = [int(x) for x in stdin.readline().split()]
+
+if a == b and b == c:
+    stdout.write('1\n')
+elif a == b or b == c or a == c:
+    stdout.write('2\n')
+else:
+    stdout.write('3\n')
