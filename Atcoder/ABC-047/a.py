998fbf77abbbf2a564dd82b44c3f0bc844316356
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-08-10 21:18:47 -0400

    Solution atcoder ABC-047-A

---
+++ Atcoder/ABC-047/a.py
@@ -0,0 +1,7 @@
+from sys import stdin, stdout
+[a, b, c] = [int(x) for x in stdin.readline().split()]
+
+if a + b == c or a + c == b or b + c == a:
+    stdout.write('Yes\n')
+else:
+    stdout.write('No\n')
