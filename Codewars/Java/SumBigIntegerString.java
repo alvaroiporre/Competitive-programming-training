61c84dcbdde4e8d5a8d989192aed29bb5d043ce6
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-01-10 13:15:31 -0400

    Add solution SumBigIntegerString codewars java

---
+++ Codewars/Java/SumBigIntegerString.java
@@ -0,0 +1,28 @@
+//https://www.codewars.com/kata/5324945e2ece5e1f32000370
+public class Kata {
+  public static String sumStrings(String a, String b) {
+    int sa = a.length();
+    int sb = b.length();
+    int sizeSol = Math.max(sa,sb);
+    String sol = "";
+    String rest = "0";
+    for(int i = sizeSol - 1; i >= 0; i--) {
+      int aAux = (sa - 1 >= 0? Integer.parseInt(a.substring(sa - 1, sa--)):0);
+      int bAux = (sb - 1 >= 0? Integer.parseInt(b.substring(sb - 1, sb--)):0);
+      int n = Integer.parseInt(rest) + aAux + bAux;
+      if(n < 10){
+        sol = Integer.toString(n) + sol;
+        rest = "0";
+      } else {
+        rest = Integer.toString((int)(n/10));
+        sol = Integer.toString(n%10) + sol;
+      }
+    }
+    if(!rest.equals("0")) sol = rest + sol;
+    int i = 0;
+    while(sol.substring(i,i+1).equals("0")){
+      i++;
+    }
+    return sol.substring(i);
+  }
+}
