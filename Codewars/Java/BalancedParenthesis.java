5fb235f95c66b6d4cc6984612fbdef8f6473f54b
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-01-12 10:56:09 -0400

    Update solution BalancedParenthesis codewars java

--- Codewars/Java/BalancedParenthesis.java
+++ Codewars/Java/BalancedParenthesis.java
@@ -4,16 +4,8 @@ import java.util.*;
 public class BalancedParens {
     static void generateParenthesis(int left, int right, String s, List<String> answer) {
         if (left == 0 && right == 0) answer.add(s);
- 
-        if (left > right || left < 0 || right < 0) return;
- 
-        s += "(";
-        generateParenthesis(left - 1, right, s, answer);
-        s = s.substring(0, s.length() - 1);
- 
-        s += ")";
-        generateParenthesis(left, right - 1, s, answer);
-        s = s.substring(0, s.length() - 1);
+        if (left > 0)generateParenthesis(left - 1, right, s + "(", answer);
+        if (right > 0 && left < right)generateParenthesis(left, right - 1, s + ")", answer);
     }
   
     public static List <String> balancedParens (int n) {
